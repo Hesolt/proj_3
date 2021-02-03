5028ec0f9d3d5bcc8fa2e635786d3a0b695c3232
Yuriy Maryshev <modiado@yandex.ru>
2021-02-03 13:27:05 +0700

    Update UnitTest1.cs

--- proj_3_test/UnitTest1.cs
+++ proj_3_test/UnitTest1.cs
@@ -9,5 +9,10 @@ namespace proj_3_test
         {
             Assert.That(proj_3.Program.Calc(10, 5), Is.EqualTo(50));
         }
+        [Test]
+        public void Test2()
+        {
+            Assert.That(proj_3.Program.Calc(50, 5), Is.EqualTo(150));
+        }
     }
 }
